7381014f4a3ab1282507296316e13bb23d012b76
BryanFG01 <bryanfg@hotmail.es>
2024-05-18 10:37:51 -0500

    Actualizacion de datos js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,6 @@
 document.getElementById("btn-show-all").addEventListener("click", function () {
   alert("¡Hola! Gracias por visitar mi página web.");
 });
+
+let elemntButton = document.getElementById("btn-show-all");
+elemntButton.innerHTML = "Contactanos";
